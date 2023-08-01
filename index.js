cba6468b1100c8d055cd4b2d6195d2b677eb1dde
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-08-02 05:21:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function plst(json) {
 			urlfee = urlfe.replace(".blogspot.com/feeds/","@").replace(Base64.decode("d3d3LmJsb2dnZXIuY29tL2ZlZWRzLzc5MTk1MDYwMDMzNDY3MjAzOQ=="),Base64.decode("ZmlsZXMtZ29vZ2xlLWNvZGUtZm9yLW15LXMwY2lhbC1tZWRpYS5ibG9nc3BvdC5jb20vZmVlZHM=")).replace(Base64.decode("ZHJhZnQuYmxvZ2dlci5jb20vZmVlZHMvNzkxOTUwNjAwMzM0NjcyMDM5"),Base64.decode("ZmlsZXMtZ29vZ2xlLWNvZGUtZm9yLW15LXMwY2lhbC1tZWRpYS5ibG9nc3BvdC5jb20vZmVlZHM=")),
 			urlfeed = Base64.encode(urlfee),
 			ktn = Base64.encode(urlfeed).replace("YUhSMGNITTZMeTltYVd4bGN5MW5iMjluYkdVdFkyOWtaUzFtYjNJdGJYa3RjekJqYVdGc0xXMWxaR2xoTG1Kc2IyZHpjRzkwTG1OdmJTOW1aV1ZrY3k5d2IzTjBjeTlrWldaaGRXeDBMe","%"), pgntn = "p/stream.html?m=1#";
-	othrPrt += '<li>'+ktn+'</li>';
+		othrPrt += '<li>'+ktn+'</li>';
+	}		
 	document.getElementById('ListPart').innerHTML = '<ul>'+othrPrt+'</ul>';	     
-}
 }
