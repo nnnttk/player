da1d3ec255e757ee4fd94ca764a4c18fbbe27e29
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-11-06 21:44:36 +0700

    Update cookie_auto.js

--- cookie_auto.js
+++ cookie_auto.js
@@ -65,8 +65,8 @@ function sepuluhdtk() {
 }
 
 function sepuluhdtk2() { 
-      document.getElementById('actvL').href='https://www.niceoffer.xyz/'; 
-      document.getElementById('adstrr').src='https://www.niceoffer.xyz/'; 
+      document.getElementById('actvL').href='https://viicentr.com/dc/?blockID=344737&tb=https%3A%2F%2Fwatch.idblog.eu.org'; 
+      document.getElementById('adstrr').src='https://viicentr.com/dc/?blockID=344737&tb=https%3A%2F%2Fwatch.idblog.eu.org'; 
 }
 
 var num = hitunganviewnya;
