f4f5dc05623e336d6a6795f3701c596f279d7ccc
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-12-23 16:26:25 +0700

    Update index.js

--- v4/index.js
+++ v4/index.js
@@ -1,6 +1,6 @@
 const IdCode = window.location.href.replace(/ht.+de=/,"").replace("#","").replace(/\&fbclid.+/,"").toUpperCase();  
-const DoHost = window.location.hostname;
-const apiUrl = `${DoHost}/v4/data/${IdCode}.json`,
+const DoHost = window.location.protocol+window.location.hostname+window.location.pathname;
+const apiUrl = `${DoHost}data/${IdCode}.json`,
     resultContainer = document.getElementById("result-container");
 fetch(apiUrl).then((t => {
     if (!t.ok) throw new Error(`HTTP error! Status: ${t.status}`);
