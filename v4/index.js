644f16081e838c5dc122e8ad653d7e644d17a99f
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-12-23 17:13:57 +0700

    Update index.js

--- v4/index.js
+++ v4/index.js
@@ -1,5 +1,4 @@
 const IdCode = window.location.href.replace(/ht.+de=/,"").replace("#","").replace(/\&fbclid.+/,"").toUpperCase();  
-if (IdCode != "") {alert("DELETED")}
 const DoHost = window.location.protocol+'//'+window.location.hostname+window.location.pathname;
 const apiUrl = `${DoHost}data/${IdCode}.json`,
     resultContainer = document.getElementById("result-container");
@@ -43,5 +42,5 @@ function loadVideo(videoId,IdCode,firstLetter) {
 
     
 })).catch((t => {
-    console.error("Fetch error:", t)
+    window.location=window.location.protocol+'//'+window.location.hostname+'/home';
 }));  
