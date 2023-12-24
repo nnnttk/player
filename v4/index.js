73cc88b26b6a159252241dfef3a8b7d9c6bd152b
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-12-24 10:05:18 +0700

    Update index.js

--- v4/index.js
+++ v4/index.js
@@ -44,3 +44,47 @@ function loadVideo(videoId,IdCode,firstLetter) {
 })).catch((t => {
     window.location=window.location.protocol+'//'+window.location.hostname+'/home';
 }));  
+
+
+fetch('list.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the JSON data
+            console.log(data);
+	    for (var i = 0; i < data.filmTitles.length; i++) {
+		var posts = ``;
+                const dataJudul = `${DoHost}data/${data.filmTitles[i].toUpperCase()}.json`; 
+		const dJforScrp = data.filmTitles[i].replace('-','');
+		const jsData = " function Data"+i+"("+dJforScrp+"){ \n\
+					var isPosts"+i+" = ''; \n\
+					for (var i2 = 0; i2 < 1; i2++) { \n\
+        				const gmbrthumbnailImg = "+dJforScrp+".result.files[i2].single_img; \n\
+					var gmbrIMG = encodeURIComponent(gmbrthumbnailImg); \n\
+					} \n\
+					isPosts"+i+" += decodeURIComponent('%3Cdiv%20class%3D%22result-item%22%3E%3Ca%20id%3D%22"+data.filmTitles[i].toUpperCase()+"%22%20href%3D%22.%2F%3Fcode%3D"+data.filmTitles[i].toUpperCase()+"%22%3E%3Cimg%20src%3D%22'+gmbrIMG+'%22%20alt%3D%22"+data.filmTitles[i].toUpperCase()+"%22%20%2F%3E%3C%2Fa%3E%3Ca%20href%3D%22.%2F%3Fcode%3D"+data.filmTitles[i].toUpperCase()+"%22%3E"+data.filmTitles[i].toUpperCase()+"%3C%2Fa%3E%3C%2Fdiv%3E'); \n\
+					$('#filmList').append(isPosts"+i+");	\n\
+					 \n\
+				} \n\
+				\n\
+				$(document).ready(function() {\n\
+    					$.ajax({\n\
+        					url: '"+dataJudul+"',\n\
+        					dataType: 'json',\n\
+        					success: function("+dJforScrp+") {Data"+i+"("+dJforScrp+")},\n\
+        					error: function(xhr, status, error) {\n\
+            						console.error(status + ': ' + error);\n\
+            						$('#filmList').text('Error fetching JSON data.');\n\
+        						}\n\
+    					});\n\
+				});";
+
+		posts += jsData;   
+		$('#scriptc').append('<scr'+'ipt>'+posts+'</scr'+'ipt>');
+            }
+    
+        });
