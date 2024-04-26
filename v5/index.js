82f37e2955f69583d35039adf0ed8ebbd99f5a24
nnnttk <126535843+nnnttk@users.noreply.github.com>
2024-04-26 10:51:39 +0700

    Update index.js

--- v5/index.js
+++ v5/index.js
@@ -42,7 +42,7 @@ function loadVideo(videoId,IdCode,p) {
 
 
 
-fetch('https://cdn.jsdelivr.net/gh/nnnttk/d@main/repair/lists5.json')
+fetch('https://cdn.jsdelivr.net/gh/nnnttk/d@main/repair/list.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
